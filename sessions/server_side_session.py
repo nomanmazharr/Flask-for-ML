7b7e93fdf68d2d7b27cdc5262db9e1631ede4534
Noman <nmazhar911@gmail.com>
2024-09-20 16:16:29 +0500

    using server side session to enhance security

---
+++ sessions/server_side_session.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, url_for, session, redirect, flash, request
+from forms import LoginForm
+from flask_session import Session
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = 'secret'
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', title='Home')
+
+
+@app.route('/about')
+def about():
+    if "username" not in session:
+        flash('Login Required')
+        return redirect(url_for('Login', next=request.url))
+    else:
+        flash(f"Hi {session['username']}! Welcome to the app...")
+    return render_template('about.html', title="About")
+
+
+@app.route('/contact')
+def contact():
+    if "username" not in session:
+        flash('Login Required')
+        return redirect(url_for('Login', next = request.url))
+    else:
+        flash(f"Hi {session['username']}! Welcome to the app...")
+    return render_template('contact.html', title='Contact')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def Login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['username'] = form.email.data
+        flash(f"Successfully logged in as {session['username'].title()}")
+        next_url = request.args.get('next')
+        return redirect(next_url or url_for('home') )
+    
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

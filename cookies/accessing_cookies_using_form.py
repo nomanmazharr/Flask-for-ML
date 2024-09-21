2279a88d97b2131c9230da45a8458c4e3d7a6a4e
Noman <nmazhar911@gmail.com>
2024-09-21 11:05:24 +0500

    accessing data through forms and returning as cookies to the user

--- cookies/accessing_cookies_using_form.py
+++ cookies/accessing_cookies_using_form.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, session, redirect, flash, request
+from flask import Flask, render_template, url_for, make_response, redirect, flash, request
 from forms import LoginForm
 
 app = Flask(__name__)
@@ -13,21 +13,23 @@ def home():
 
 @app.route('/about')
 def about():
-    if "username" not in session:
+    user = request.cookies.get('username')
+    if user is None:
         flash('Login Required')
         return redirect(url_for('Login', next=request.url))
     else:
-        flash(f"Hi {session['username']}! Welcome to the app...")
+        flash(f"Hi {user}! Welcome to the app...")
     return render_template('about.html', title="About")
 
 
 @app.route('/contact')
 def contact():
-    if "username" not in session:
+    user = request.cookies.get('username')
+    if user is None:
         flash('Login Required')
         return redirect(url_for('Login', next = request.url))
     else:
-        flash(f"Hi {session['username']}! Welcome to the app...")
+        flash(f"Hi {user}! Welcome to the app...")
     return render_template('contact.html', title='Contact')
 
 
@@ -35,10 +37,14 @@ def contact():
 def Login():
     form = LoginForm()
     if form.validate_on_submit():
-        session['username'] = form.email.data
-        flash(f"Successfully logged in as {session['username'].title()}")
-        next_url = request.args.get('next')
-        return redirect(next_url or url_for('home') )
+        username = form.email.data
+        response = make_response('')
+        response.set_cookie('username', username)
+        flash(f"Successfully logged in as {username}!")
+        next_url = request.args.get('next') or url_for('home')
+        response.headers["LOCATION"] = next_url
+        response.status_code = 303 # redirecting user to any page 
+        return response
     
     return render_template('login.html', title='Login', form=form)
 
